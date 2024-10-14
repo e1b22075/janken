70a2af71b7e628de3ebc21ddfd08d0d13429c897
hirao mitsuki <e1b22075@oit.ac.jp>
2024-10-14 15:48:59 +0900

    認証機能の実装

--- src/main/java/oit/is/z2354/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2354/kaizi/janken/controller/JankenController.java
@@ -1,5 +1,8 @@
 package oit.is.z2354.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z2354.kaizi.janken.model.Entry;
+
 @Controller
 public class JankenController {
+
+  @Autowired
+  private Entry entry;
 
   @GetMapping("/janken")
   public String sample21() {
@@ -16,10 +24,11 @@ public class JankenController {
   }
 
   @PostMapping("/janken")
-  public String janken(@RequestParam String janken, ModelMap model) {
-    String name = janken;
-
-    model.addAttribute("name", name);
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
     return "janken.html";
   }
 
